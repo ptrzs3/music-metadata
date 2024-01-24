d413448c99b0e7ef05a032755a2f574901100229
zhangsan <zhangsan8086@gmail.com>
2024-01-24 12:03:37 +0800

    update example

--- examples/flac_example.rs
+++ examples/flac_example.rs
@@ -1,6 +1,6 @@
 use music_metadata::FlacParser;
 fn main() -> std::io::Result<()> {
-    let mut parser = FlacParser::new(r"C:\Users\ptrzs\Desktop\q\qqqg.flac")?;
+    let mut parser = FlacParser::new(r"C:\Users\ptrzs\Desktop\q\mix.flac")?;
     parser.parse()?;
     Ok(())
 }
